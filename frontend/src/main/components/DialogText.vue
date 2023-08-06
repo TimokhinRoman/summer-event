1f2d349140ed4aef9047dd7a3acd4601d266a7c0
timohin-ro-170605 <timohin-ro-170605@fsvps.ru>
2023-08-06 23:27:48 +0300

    Стили для стихов

--- frontend/src/main/components/DialogText.vue
+++ frontend/src/main/components/DialogText.vue
@@ -16,3 +16,36 @@ export default {
   }
 }
 </script>
+
+<style>
+.poem {
+  margin: auto;
+  font-family: Baskerville;
+  font-size: 14px;
+  text-align: left;
+}
+
+.poem p {
+  margin-bottom: 15px;
+  line-height: 1.45em;
+}
+
+.poem p:first-child {
+  margin-top: 0;
+}
+
+.poem p:first-child:first-letter {
+  float: left;
+  font-size: 40px;
+  margin: 12px 5px 0 0;
+}
+
+.poem p:first-line {
+  font-variant: small-caps;
+  letter-spacing: 1px;
+}
+
+.poem p:last-child {
+  margin-bottom: 0;
+}
+</style>
